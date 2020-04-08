1a0d716adcb59424b06b4bf23b7b4cdd704426b3
lpd <850835326@qq.com>
2020-04-08 15:06:14 +0800

    third commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,12 @@
 /**
  * @Auther: lpd
  * @Date: 2020/4/8 14:23
- * @Description:
+ * @Description:git上传试炼
  */
 public class HelloWorld {
 
     public static void main(String[] args) {
+        System.out.println("Hello Git");
         System.out.println("HelloWorld!-forGit");
     }
 }
